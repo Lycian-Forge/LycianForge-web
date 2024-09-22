771e8e9711a2c020f5704b1693d3e6887544e12e
Arda Güngör <ardagungor07@gmail.com>
2024-09-22 20:48:33 +0300

    revert commit

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-document.getElementById('popupButton').onclick = function() {
-    document.getElementById('popup').style.display = 'flex';
-};
 
-document.getElementById('closeButton').onclick = function() {
-    document.getElementById('popup').style.display = 'none';
-};
-
-window.onclick = function(event) {
-    const popup = document.getElementById('popup');
-    if (event.target === popup) {
-        popup.style.display = 'none';
-    }
-};
